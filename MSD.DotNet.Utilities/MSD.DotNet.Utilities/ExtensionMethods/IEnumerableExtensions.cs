38f146efc74e5e1ec6c2702a83b808148f9052b6
Marc Duiker <marcduiker@gmail.com>
2013-06-10 20:49:47 +0200

    First commit IsNullOrEmpty for IEnumerable

---
+++ MSD.DotNet.Utilities/MSD.DotNet.Utilities/ExtensionMethods/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MSD.DotNet.Utilities.ExtensionMethods
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+    }
+}
