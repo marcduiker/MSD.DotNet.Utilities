953e8a3037a343ed9c15a023a793b73231fe2db3
Marc Duiker <marcduiker@gmail.com>
2013-06-10 22:17:10 +0200

    Added unittests for IsNullOrEmpty

---
+++ MSD.DotNet.Utilities/MSD.DotNet.Utilities.UnitTests/IEnumerableExtensionTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using MSD.DotNet.Utilities.ExtensionMethods;
+using NUnit.Framework;
+
+namespace MSD.DotNet.Utilities.UnitTests
+{
+    public class IEnumerableExtensionTests
+    {
+        [Test]
+        public void IsNullOrEmpty_IEnumerableIsNull_ReturnsTrue()
+        {
+            // Arrange
+            IEnumerable<string> list = null;
+
+            // Act
+            bool result = list.IsNullOrEmpty();
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsNullOrEmpty_IEnumerableIsEmpty_ReturnsTrue()
+        {
+            // Arrange
+            IEnumerable<string> list = new List<string>();
+
+            // Act
+            bool result = list.IsNullOrEmpty();
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsNullOrEmpty_IEnumerableIsNotEmpty_ReturnsFalse()
+        {
+            // Arrange
+            IEnumerable<string> list = new List<string> { "abc" };
+
+            // Act
+            bool result = list.IsNullOrEmpty();
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
